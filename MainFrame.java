b9c871fb098197b3e96e44dcb6f12da18a41a863
Maria Eduarda <silvamariaeduarda92@gmail.com>
2024-05-15 13:50:51 -0300

    delete funcion

--- MainFrame.java
+++ MainFrame.java
@@ -4,6 +4,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 
 import java.awt.event.ActionEvent;
@@ -25,6 +26,10 @@ public class MainFrame extends JFrame{
         JButton btTask = new JButton("New Task");
         add(btTask);
 
+        JButton btDelete = new JButton("Delete Task");
+        add(btDelete);
+
+        // Ação do botão de criar task
         MainFrame self = this;
         btTask.addActionListener(new ActionListener(){
             @Override
@@ -34,6 +39,21 @@ public class MainFrame extends JFrame{
                 frame.setVisible(true);
             }
         });
+
+        //Ação do botão de deletar task
+        btDelete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                int selectedIndex = displayList.getSelectedIndex();
+                if(selectedIndex != -1){
+                    tasks.remove(selectedIndex);
+                    displayList.setListData(tasks.toArray(new String[0]));
+                }else{
+                    JOptionPane.showMessageDialog(null, "Select a task!");
+                }
+            }
+        });
+        
         add(scrollPane);
         pack();
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
